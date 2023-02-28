fb528982a44541ea9924978737dd9dd9d0625c84
ertugrulayranci <ertugrulayranci@gmail.com>
2023-02-28 11:14:51 +0300

    second draft

--- src/App.js
+++ src/App.js
@@ -2,11 +2,25 @@ import React, { useState } from "react";
 
 function App() {
   const [todoText, settodoText] = useState("");
-  const handleSubmit=(event)=>{event.preventDefault()
-  if(todoText ===""){alert("Please type your todo!")
-return}
-console.log(todoText)
-  }
+  const [todos, setTodos] = useState([]);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (todoText === "") {
+      alert("Please type your todo!");
+      return;
+    }
+    console.log(todoText);
+    const newTodo = {
+      id: new Date().getTime,
+      title: todoText,
+      date: new Date(),
+      hasDone: false,
+    };
+    console.log(newTodo);
+    setTodos([...todos, newTodo]);
+    settodoText("");
+  };
   return (
     <div className="container my-5">
       <form onSubmit={handleSubmit}>
@@ -24,6 +38,50 @@ console.log(todoText)
           </button>
         </div>
       </form>
+      <div className="container">
+        {todos.length === 0 ? (
+          <p className="text-center"> You don't have any todos </p>
+        ) : (
+          <>
+            {todos.map((item, index) => (
+              <div
+                key={index}
+                style={{ borderBottom: "1px solid gray" }}
+                className="d-flex justify-content-between align-items-center"
+              >
+                <div>
+                  <h1
+                    style={{
+                      textDecoration:
+                        item.hasDone === true ? "line-through" : "none",
+                    }}
+                  >
+                    {item.title}{" "}
+                  </h1>
+                  <small>{new Date(item.date).toLocaleDateString()}</small>
+                </div>
+                <div>
+                  <button
+                    onClick={() => {
+                      const filteredTodos = todos.filter(
+                        (i) => i.id !== item.id
+                      );
+                      setTodos(filteredTodos);
+                    }}
+                    className="btn btn-sm btn-danger"
+                  >
+                    Delete
+                  </button>
+                  <button className="btn btn-sm btn-secondary">Edit</button>
+                  <button className="btn btn-sm btn-success">
+                    {item.hasDone === false ? "Done" : "Undone"}
+                  </button>
+                </div>
+              </div>
+            ))}
+          </>
+        )}
+      </div>
     </div>
   );
 }
