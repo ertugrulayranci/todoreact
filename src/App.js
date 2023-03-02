aa80a6b60468717e80add85ced5bd6084dad2e13
ertugrulayranci <ertugrulayranci@gmail.com>
2023-03-02 11:58:19 +0300

    2nd draft

--- src/App.js
+++ src/App.js
@@ -1,81 +1,94 @@
 import React, { useState } from "react";
 
 function App() {
-  const [todoText, settodoText] = useState("");
-  const [todos, setTodos] = useState([]);
-
+  const [todoText, setTodoText] = useState("");
+  const [todos,setTodos]=useState([])
   const handleSubmit = (event) => {
     event.preventDefault();
+    /*validation*/
     if (todoText === "") {
       alert("Please type your todo!");
       return;
     }
-    console.log(todoText);
-    const newTodo = {
-      id: new Date().getTime,
+    const newTodo={
+      id:new Date().getTime(),
       title: todoText,
       date: new Date(),
-      hasDone: false,
-    };
-    console.log(newTodo);
-    setTodos([...todos, newTodo]);
-    settodoText("");
+      hasDone: false
+    }
+    setTodos([...todos,newTodo])
+    setTodoText("")
   };
+  const deleteTodo=(id)=>{
+    const filteredTodos=todos.filter
+    ((i) => i.id !== id);
+    setTodos(filteredTodos);
+    }
+
+    const changeHasDone =(todo)=>{
+      console.log(todo)
+      let tempTodos=[]
+      for(let i=0; i<todos.length; i++){
+        if(todos[i].id==todo.id){
+          let updatedTodo={
+            ...todo,
+            hasDone: !todo.hasDone
+          }
+          tempTodos.push(updatedTodo)
+        }else{
+          tempTodos.push(todos[i])
+        }
+      }
+      setTodos(tempTodos)
+    }  
   return (
     <div className="container my-5">
       <form onSubmit={handleSubmit}>
         <div className="input-group mb-3">
           <input
             value={todoText}
-            onChange={(event) => [settodoText(event.target.value)]}
+            onChange={(event) => {
+              setTodoText(event.target.value);
+            }}
             type="text"
             className="form-control"
             placeholder="Type your todo"
-            aria-label="Type your todo"
           />
-          <button className="btn btn-primary w-25" type="Submit">
+          <button className="btn btn btn-primary w-25" type="submit">
             ADD
           </button>
         </div>
       </form>
       <div className="container">
         {todos.length === 0 ? (
-          <p className="text-center"> You don't have any todos </p>
+          <p className="text-center"> You don't have any todos yet.</p>
         ) : (
           <>
             {todos.map((item, index) => (
-              <div
-                key={index}
-                style={{ borderBottom: "1px solid gray" }}
-                className="d-flex justify-content-between align-items-center"
-              >
+              <div key={index} style={{borderBottom:"1px solid gray"}}className="d-flex justify-content-between align-items-center">
                 <div>
                   <h1
                     style={{
                       textDecoration:
                         item.hasDone === true ? "line-through" : "none",
                     }}
+                    
                   >
                     {item.title}{" "}
                   </h1>
                   <small>{new Date(item.date).toLocaleDateString()}</small>
                 </div>
                 <div>
-                  <button
-                    onClick={() => {
-                      const filteredTodos = todos.filter(
-                        (i) => i.id !== item.id
-                      );
-                      setTodos(filteredTodos);
-                    }}
-                    className="btn btn-sm btn-danger"
-                  >
-                    Delete
-                  </button>
-                  <button className="btn btn-sm btn-secondary">Edit</button>
-                  <button className="btn btn-sm btn-success">
-                    {item.hasDone === false ? "Done" : "Undone"}
-                  </button>
+                 <button onClick={()=>{deleteTodo(item.id)}
+                 
+                }className="btn btn-sm btn-danger">Delete</button>
+                
+                <button className="btn btn-sm btn-secondary">Edit</button>
+                <button  
+                  onClick={()=>changeHasDone(item)}
+                  className="btn btn-sm btn-success"> 
+                  {item.hasDone === false ? "Done" : "Undone"}
+                </button>
                 </div>
               </div>
             ))}
@@ -85,5 +98,4 @@ function App() {
     </div>
   );
 }
-
 export default App;
