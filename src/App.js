b52bf45af795fdee0284ffa8ae4a04c093da0624
ertugrulayranci <ertugrulayranci@gmail.com>
2023-03-02 12:27:50 +0300

    3rd draft with edit

--- src/App.js
+++ src/App.js
@@ -28,7 +28,18 @@ function App() {
     const changeHasDone =(todo)=>{
       console.log(todo)
       let tempTodos=[]
-      for(let i=0; i<todos.length; i++){
+      todos.map((item,index)=>{
+        if(item.id === todo.id) {
+          let updatedTodo={
+            ...todo,
+            hasDone: !todo.hasDone
+          }
+          tempTodos.push(updatedTodo)
+        }else{
+          tempTodos.push(item)
+        }
+      })
+      /* for(let i=0; i<todos.length; i++){
         if(todos[i].id==todo.id){
           let updatedTodo={
             ...todo,
@@ -38,7 +49,7 @@ function App() {
         }else{
           tempTodos.push(todos[i])
         }
-      }
+      } */
       setTodos(tempTodos)
     }  
   return (
@@ -59,36 +70,56 @@ function App() {
           </button>
         </div>
       </form>
+      <form>
+        <div className="input-group mb-3">
+          <input className="form-control" type="text" />
+          <button className="btn btn btn-info w-25" type="submit">
+            Edit
+          </button>
+          <button className="btn btn btn-danger w-25" type="button">
+            Cancel
+          </button>
+        </div>
+      </form>
       <div className="container">
         {todos.length === 0 ? (
           <p className="text-center"> You don't have any todos yet.</p>
         ) : (
           <>
             {todos.map((item, index) => (
-              <div key={index} style={{borderBottom:"1px solid gray"}}className="d-flex justify-content-between align-items-center">
+              <div
+                key={index}
+                style={{ borderBottom: "1px solid gray" }}
+                className="d-flex justify-content-between align-items-center"
+              >
                 <div>
                   <h1
                     style={{
                       textDecoration:
                         item.hasDone === true ? "line-through" : "none",
                     }}
-                    
                   >
                     {item.title}{" "}
                   </h1>
                   <small>{new Date(item.date).toLocaleDateString()}</small>
                 </div>
                 <div>
-                 <button onClick={()=>{deleteTodo(item.id)}
-                 
-                }className="btn btn-sm btn-danger">Delete</button>
-                
-                <button className="btn btn-sm btn-secondary">Edit</button>
-                <button  
-                  onClick={()=>changeHasDone(item)}
-                  className="btn btn-sm btn-success"> 
-                  {item.hasDone === false ? "Done" : "Undone"}
-                </button>
+                  <button
+                    onClick={() => {
+                      deleteTodo(item.id);
+                    }}
+                    className="btn btn-sm btn-danger"
+                  >
+                    Delete
+                  </button>
+
+                  <button className="btn btn-sm btn-secondary">Edit</button>
+                  <button
+                    onClick={() => changeHasDone(item)}
+                    className="btn btn-sm btn-success"
+                  >
+                    {item.hasDone === false ? "Done" : "Undone"}
+                  </button>
                 </div>
               </div>
             ))}
